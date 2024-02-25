df730d8cae24f3d4807afcfb0c3e5c1c23740236
Dalleth <essi_dalleth@outlook.com>
2024-02-25 20:18:56 -0300

    desafio - Flow Control

---
+++ flow-control/src/Contador.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Contador {
+   public static void main(String[] args) {
+
+		Scanner terminal = new Scanner(System.in);
+
+		System.out.println("Digite o primeiro parâmetro: ");
+		int parametroUm = terminal.nextInt();
+
+		System.out.println("Digite o segundo parâmetro: ");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException e) {
+			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+		
+		int contagem = parametroDois - parametroUm;
+		//realizar o for para imprimir os números com base na variável contagem
+        for (int i = parametroUm; i <= parametroDois; i++) {
+            System.out.println(i);
+        }
+        
+	}
+}
+
+
+// criação da classe que representa a exceção do negocio
+
+class ParametrosInvalidosException extends RuntimeException {
+    
+}
